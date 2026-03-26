c18d48b22165f5dbe8e204e334bb71d8c9246354
blessware <blesswared@gmail.com>
2026-03-26 18:47:05 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,11 +1,30 @@
-const { Client, GatewayIntentBits } = require('discord.js');
+const {
+  Client,
+  GatewayIntentBits,
+  ActivityType
+} = require('discord.js');
 
 const client = new Client({
-  intents: [GatewayIntentBits.Guilds]
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers]
 });
 
 client.once('ready', () => {
-  console.log("BOT ONLINE");
+  console.log(`blessed online as ${client.user.tag}`);
+
+  client.user.setPresence({
+    status: 'dnd',
+    activities: [{ name: '@tryingmybest', type: ActivityType.Playing }]
+  });
+});
+
+// TEST BUTTON HANDLER
+client.on('interactionCreate', async interaction => {
+  if (!interaction.isButton()) return;
+
+  await interaction.reply({
+    content: "Button works.",
+    ephemeral: true
+  });
 });
 
 client.login(process.env.TOKEN);
